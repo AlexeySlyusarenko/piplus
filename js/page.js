b1c002f05522cb82b882aedcb267de5d51be56de
Alexey Slyusarenko <slyusarenko.a.n@gmail.com>
2019-01-04 12:52:41 +0300

    Test debug function

--- js/page.js
+++ js/page.js
@@ -79,7 +79,7 @@ class PageObj {
 
         debugElem.innerHTML = `${arguments[0]}`;
 
-        for (let i = 1; i < arguments[i]; i++) {
+        for (let i = 1; i < arguments.length; i++) {
             debugElem.innerHTML = debugElem.innerHTML +
                                 `<br>${arguments[i]}`
         }
